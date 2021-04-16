4f6603fab0d7fdf7b2a82e7ee6c07f7c82a925dc
StudyObolonskiy <study.obolonskiy@gmail.com>
2021-04-16 15:21:21 +0200

    fix: fix header menu

--- online-zoo/script.js
+++ online-zoo/script.js
@@ -1,4 +1,7 @@
 let menuBtn = document.querySelector('.menu-btn');
+let headerWrapper = document.querySelector('.header__wrapper');
+let logo = document.querySelector('.logo')
+let logoImg = document.querySelector('.logo__img')
 menuBtn.addEventListener('click', e => {
   let mobileNav = document.querySelector('.mobile__nav');
   if (mobileNav.style.display === 'flex') {
@@ -7,4 +10,7 @@ menuBtn.addEventListener('click', e => {
     mobileNav.style.display = 'flex';
   }
   menuBtn.classList.toggle('active');
+  headerWrapper.classList.toggle('header__mob');
+  logo.classList.toggle('logo_active');
+  logoImg.classList.toggle('logo__img_active');
 });
