1337f2888e1c436bc57c24811b92991442e2fb16
StudyObolonskiy <study.obolonskiy@gmail.com>
2021-06-11 11:22:13 +0200

    refactor: rename variable for better readability

--- match-match-game/src/components/base-component.ts
+++ match-match-game/src/components/base-component.ts
@@ -1,8 +1,8 @@
 export class BaseComponent {
   public readonly element: HTMLElement;
 
-  constructor(tag: keyof HTMLElementTagNameMap = 'div', styles: string[] = []) {
+  constructor(tag: keyof HTMLElementTagNameMap = 'div', classList: string[] = []) {
     this.element = document.createElement(tag);
-    this.element.classList.add(...styles);
+    this.element.classList.add(...classList);
   }
 }
