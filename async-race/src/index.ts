ca9166756e30b8858a9a1014b48876535f7769e4
StudyObolonskiy <study.obolonskiy@gmail.com>
2021-06-13 16:25:49 +0200

    feat: implement main application component

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,3 +1,10 @@
+import { App } from './app';
 import './styles.scss';
 
-console.log('Hello world');
+window.onload = () => {
+  const appElement = document.body;
+
+  if (!appElement) throw new Error('App root element not found');
+  const app = new App(appElement);
+  app.start();
+};
